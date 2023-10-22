fbc9b277cbc702505d4b89f4acb2cf9d7706bb52
jorwin <jorwin@Xubuntu.myguest.virtualbox.org>
2023-10-22 12:36:29 -0600

    Build left sidebar

---
+++ react-portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,53 @@
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+import LogoS from '../../assets/images/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import {faLinkedIn, faGithub, faYoutube, faSpotify } from '@fortawesome/free-brand-svg-icons'
+
+const Sidebar = () => {
+    return (
+        <div className='nav-bar'>
+            <Link className='logo' to='/'>
+                <img src={LogoS} alt='logo' />
+                <img className='sub-logo' src={LogoSubtitle} alt='logo_subtitle' />
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to='/'>
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" to='/about'>
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" to='/contact'>
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/jorwin-reyes-191b26257?'>
+                        <FontAwesomeIcon icon={faLinkedIn} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://github.com/Jorwin-dev'>
+                        <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://www.youtube.com/channel/UCtsHvGC3eieHlmHQf_tYw-A'>
+                        <FontAwesomeIcon icon={faYoutube} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a target='_blank' rel='noreferrer' href='https://open.spotify.com/user/xq5kq4lhy43au02hemx86tnct'>
+                        <FontAwesomeIcon icon={faSpotify} color='#4d4d4e' />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar
